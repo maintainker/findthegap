9a8cad4b5de2f73df15c243472d8be1a1d815c9a
ssul <younghwan.sul@wehuddling.com>
2026-06-20 17:00:22 +0900

    setting: create project & m-f setting

---
+++ findthegap/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: "findthegapApp",
+      remotes: {
+        remoteApp: "http://localhost:5173/assets/remoteEntry.js",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    target: "esnext",
+  },
+  server: {
+    port: 5000,
+  },
+});
